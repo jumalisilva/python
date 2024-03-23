6dd0c93dddc231604782f2ec92285e7a191015ac
jumalisilva <juliamalisilva@gmail.com>
2024-03-23 11:30:06 -0300

    Atividade 01

---
+++ atividade01.py
@@ -0,0 +1,11 @@
+# Nome do usuário
+nome = input('Digite o seu nome: ')
+print('Bom dia', nome)
+
+# Valores
+val1 = float(input('Digite o primeiro número: '))
+val2 = float(input('Digite o segundo número: '))
+print('val1 + val2 =', val1 + val2)
+print('val1 - val2 =', val1 - val2)
+print('val1 x val2 =', val1 * val2)
+print('val1 / val2 =', val1 / val2)
