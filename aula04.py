4f20d9822f11ed50f5dd27ab8261bea215014724
jumalisilva <juliamalisilva@gmail.com>
2024-04-20 11:04:38 -0300

    Aula 04

--- aula04.py
+++ aula04.py
@@ -1,6 +1,6 @@
 # Laços de repetição
 
-## 1. While - executa o laço ate que a condição seja falsa
+## 1. While - executa o laço até que a condição seja falsa
 i = 0 # crio uma variável e guardo zero
 while (i < 5): # verifica: enquanto a variável i for menor que 5
     print(i)
