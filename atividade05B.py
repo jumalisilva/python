33df7a68fdbcfeacc5e8409991d9a8bcadaedacd
jumalisilva <juliamalisilva@gmail.com>
2024-04-20 11:50:43 -0300

    Atividade 05

--- atividade05B.py
+++ atividade05B.py
@@ -1,2 +1,9 @@
 # Faça um programa que solicite ao usuário que insira uma senha. O programa deve permitir que o usuário tente entrar com a senha até que ele acerte a senha correta.
 
+senhaUsuario = int(input('Digite uma senha numérica: '))
+senhaCorreta = 12345
+
+if (senhaUsuario == senhaCorreta):
+    print('Você acessou o sistema!')
+else:
+    print('Senha incorreta. Tente novamente.')
--- atividade05C.py
+++ atividade05C.py
@@ -1,1 +1,14 @@
 # Agora adicione ao programa anterior (atividade05B) uma tentativa máxima de 4 vezes para errar a senha
+
+senhaUsuario = int(input('Digite uma senha numérica: '))
+senhaCorreta = 12345
+i = 4
+
+while (i >= 0):
+    if (senhaUsuario != senhaCorreta):
+        print('Senha incorreta, {} tentativa(s) restantes. Tente novamente.' . format(i))
+        senhaUsuario = int(input('Digite uma senha numérica: '))
+        i -= 1
+    elif (senhaUsuario == senhaCorreta):
+        print('Você acessou o sistema!')
+        break
