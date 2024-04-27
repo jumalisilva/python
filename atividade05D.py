127b78b17c882b8ba441638e47112b42a8361b18
jumalisilva <juliamalisilva@gmail.com>
2024-04-27 09:18:17 -0300

    Atividade 05

--- atividade05D.py
+++ atividade05D.py
@@ -1,2 +1,15 @@
 # Crie um jogo em que o jogador tenta adivinhar um número. O programa deve fornecer dicas se o palpite do jogador for muito alto ou muito baixo.
 
+numeroAleatorio = int(input('Digite um número: '))
+numeroCorreto = 17
+
+
+while numeroAleatorio != numeroCorreto:
+    if (numeroAleatorio < numeroCorreto):
+        print('Quase lá, tente novamente!')
+        numeroAleatorio = int(input('Digite um número: '))
+    elif (numeroAleatorio > numeroCorreto):
+        print('Ops, passou um pouco. Tente novamente.')
+        numeroAleatorio = int(input('Digite um número: '))
+
+print('Parabéns, você acertou o número aleatório!')    
