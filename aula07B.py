e20a3711f09a02b4bf3990f6a912ef46820bd0da
jumalisilva <juliamalisilva@gmail.com>
2024-05-18 09:40:54 -0300

    Aula 07 - Importando biblioteca math

---
+++ aula07B.py
@@ -0,0 +1,13 @@
+# Importando bibliotecas
+
+# Bibliotecas são blocos de códigosprontos para otimizar e potencializar nossa codificação
+
+import math # importando a biblioteca math para o código
+
+# 1) math.ceil() serve para arredondar valores para cima
+a = math.ceil(8.5)
+print("Arredondando:", a) # retorna 9
+
+# 2) math.sqrt() serve para achar a raíz quadrada de um número
+b = math.sqrt(49)
+print("Raíz quadrada:", b) # retorna 7
